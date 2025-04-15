b80e1c7059f58a761472dd13965cd2ff1315862f
Manichandana001 <manichandanaadire@gmail.com>
2025-04-15 22:12:18 +0530

    Delete code_folder directory

--- code_folder/code_folder/portal.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header("Location: index.html");
-    exit();
-}
-echo "<h1>Welcome to the Portal, " . $_SESSION['user'] . "!</h1>";
-?>
